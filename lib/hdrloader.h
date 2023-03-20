94e3bf1065826f61084cc2d1f2603035d9ff32f2
gratuiti <294588611@qq.com>
2023-03-20 17:12:34 +0800

    raytracing

---
+++ lib/hdrloader.h
@@ -0,0 +1,15 @@
+
+
+
+class HDRLoaderResult {
+public:
+	int width, height;
+	// each pixel takes 3 float32, each component can be of any value...
+	float *cols;
+};
+
+class HDRLoader {
+public:
+	static bool load(const char *fileName, HDRLoaderResult &res);
+};
+
